6e0dec1e38325f5caee6636c218d2601552b75c8
barbarajaworowska <44527941+barbarajaworowska@users.noreply.github.com>
2018-11-18 12:09:08 +0100

    Add files via upload
    18.11.

---
+++ projekt2/index.js
@@ -0,0 +1,27 @@
+const $tekst = document.getElementById("tekst")
+const $przycisk = document.getElementById("przycisk")
+const $tablica = document.getElementById("tablica1")
+const tablica = [1,5,11,400]
+
+$przycisk.addEventListener("click", function() {
+    const valueFromInput = $tekst.value 
+    dodajDoTablicy(valueFromInput)
+})
+
+function dodajDoTablicy(value){
+    value = parseFloat(value)
+
+    if(isNaN(value)){
+        value = Math.floor(Math.random() *20 ) +1
+    }
+
+    tablica.unshift(value)
+    pokazWiadomosc(value, 0)
+    $tablica.textContent = tablica
+}
+
+function pokazWiadomosc(value, index){
+    const $rezultat = document.getElementById("rezultat")
+    const text = `dodano wartosc ${value} na pozycji ${index}`
+    $rezultat.textContent = text
+}
